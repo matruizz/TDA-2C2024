eb6efc5a135f67d1ae06c5e9eb43382ab29c5b5f
matruizz <mateoruizz1299@gmail.com>
2025-06-05 14:02:45 -0300

    Black and white v3.0.

--- tp1/black.cpp
+++ tp1/black.cpp
@@ -4,64 +4,101 @@
 
 using namespace std;
 
+int maximo(int a, int b, int c){
 
-int dec(vector<int> v, int i, int anterior){
+    if (a >= b && a >= c)
+    {
+        return a;
+    }else if(b >= a && b >= c ){
+        return b;
+    }else{
+        return c;
+    }
+
+}
+int f(vector<vector<vector<int> > > &m, vector<int> &v, int i, int inc, int dec){
+
 
     if (i == v.size())
     {
         return 0;
-    }else if (v[i] >= anterior)
+
+    }else if ((inc == -1) && (dec == -1))//-----//Si los dos estan vacios------------
     {
-        return dec(v, (i + 1), anterior);
-    }else{
-        return max(dec(v, i + 1, v[i]) + 1, dec(v, i + 1, v[i]));
+
+        m[i][inc + 1][dec + 1] = maximo(f(m, v, i + 1, i, dec) + 1, f(m, v, i + 1, inc, i) + 1, f(m, v, i + 1, inc, dec));
+        return m[i][inc + 1][dec + 1];
+
+    }else if ((inc == -1) && (dec != -1))//-----//Si solo inc esta vacio-------------
+    {
+        if (v[dec] < v[i])  //No puede entrar en dec
+        {
+            m[i][inc + 1][dec + 1] = f(m, v, i + 1, i, dec) + 1;
+            return m[i][inc + 1][dec + 1];
+        }else{              //Puede entrar en dec
+
+            m[i][inc + 1][dec + 1] = maximo(f(m, v, i + 1, i, dec) + 1, f(m, v, i + 1, inc, i) + 1, f(m, v, i + 1, inc, dec));
+            return m[i][inc + 1][dec + 1];
+        }
+    }else if((inc != -1) && (dec == -1))//------//Si solo dec esta vacio--------------
+    {
+        if (v[inc] > v[i]) //No puede entrar en inc
+        {
+            m[i][inc + 1][dec + 1] = f(m, v, i + 1, inc, i) + 1;
+            return m[i][inc + 1][dec + 1];
+        }else{             //Puede entrar en dec
+
+            m[i][inc + 1][dec + 1] = maximo(f(m, v, i + 1, i, dec) + 1, f(m, v, i + 1, inc, i) + 1, f(m, v, i + 1, inc, dec));
+            return m[i][inc + 1][dec + 1];
+        }
+    }else if ((inc != -1) && (dec != -1))//-----//Si ninguno esta vacio---------------
+    {
+        if(m[i][inc + 1][dec + 1] != -1){   //Si el resultado ya fue calculado
+
+            return m[i][inc + 1][dec + 1];
+
+        }else if((v[inc] < v[i]) && (v[dec] < v[i])){   //Solo puede entrar en increasing
+            
+            m[i][inc + 1][dec + 1] = max(f(m, v, i + 1, i, dec) + 1, f(m, v, i + 1, inc, dec));
+            return m[i][inc + 1][dec + 1];
+
+        }else if((v[dec] > v[i]) && (v[inc] > v[i])){   //Solo pude entrar en decreasing
+            
+            m[i][inc + 1][dec + 1] = max(f(m, v, i + 1, inc, i) + 1, f(m, v, i + 1, inc, dec));
+            return m[i][inc + 1][dec + 1];
+        
+        }else if((v[inc] >= v[i]) && (v[dec] <= v[i]) && (i != 0)){ //No puede entrar en ninguno
+
+            m[i][inc + 1][dec + 1] = f(m, v, i + 1, inc, dec);
+            return m[i][inc + 1][dec + 1];
+
+        }else{
+        
+            m[i][inc + 1][dec + 1] = maximo(f(m, v, i + 1, i, dec) + 1, f(m, v, i + 1, inc, i) + 1, f(m, v, i + 1, inc, dec));
+            return m[i][inc + 1][dec + 1];
+        }
     }
     
-    
-
-    return 0;
 }
-int increasing(vector<int> v, int i, int anterior, vector<int> noPintados){
-
-    if (i == v.size())
-    {
-        return dec(noPintados, 0, 100);
-    }else if ((v[i] <= anterior))
-    {
-        return increasing(v, (i + 1), anterior, noPintados);
-    }else{
-
-        vector<int> aux = noPintados;
-        
-        aux.push_back(v[i]);
-        
-        return max(increasing(v, (i + 1), v[i], aux) + 1, increasing(v, (i + 1), anterior, noPintados));
-    }
-}
-
-
-
 
 int main() {
 
     //Codigo para hacer la entrada y salida de datos mas eficiente
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
+
     int n = 0;
 
     cin >> n;
 
-    vector<vector<pair<int, int>>> datos;
+    vector<vector<int> > datos;
 
     while (n != (-1))
     {
-        vector<pair<int, int>> caso(n);
+        vector<int> caso(n);
         for (int i = 0; i < n; i++)
         {
-            int aux = 0;
-            cin >> aux;
-            caso[i] = make_pair(aux, 0);
+            cin >> caso[i];
         }
         
         datos.push_back(caso);
@@ -72,21 +109,24 @@ int main() {
         cin >> n;
     }
 
-
-    vector<int> v = {2, 3, 4, 10, 5, 6, 1};
-    vector<int> vacio;
-    int res = increasing(v, 0, -1, vacio);
-    cout << res << endl;
-
-
-    for (int i = 0; i < datos.size(); i++)
+    for (int q = 0; q < datos.size(); q++)
     {
-        for (int j = 0; j < datos[i].size(); j++)
+
+        vector<vector<vector<int> > > m (datos[q].size(),  (vector<vector<int> > (datos[q].size() + 1, vector<int> (datos[q].size() + 1))));
+        
+        for (int i = 0; i < m.size(); i++)
         {
-            cout << datos[i][j].first << ", " << datos[i][j].second << endl;
+            for (int j = 0; j < m.size(); j++)
+            {
+                for (int k = 0; k < m.size(); k++)
+                {
+                    m[i][j][k] = -1;
+                }
+                
+            }
+            
         }
-        cout << endl;
+        
+        cout << datos[q].size() - f(m, datos[q], 0, -1, -1) << endl;
     }
-    
-    
 }
