4e866419bc565353418f6ae84a9d96adc53f7f84
matruizz <mateoruizz1299@gmail.com>
2025-06-05 22:55:49 -0300

    Bipartito version 5 final pasa todos los tests.

--- tp2/Igna, Martín y la bipartitud/bipar.cpp
+++ tp2/Igna, Martín y la bipartitud/bipar.cpp
@@ -7,110 +7,114 @@ using namespace std;
 long long int j;
 
 
-bool existeJQueNoEstaEnqueueYNoFueVisitado(unordered_set<long long int> &elementosEnQueue, set<long long int> &adyacentesAi, vector<long long int> &visitado){
+bool existeJ(set<long long int> &adyacentesAi, vector<long long int> &elementosEnQueue, vector<long long int> &visitado){
 
-    set<long long int>::iterator adyacente = adyacentesAi.begin();
+    set<long long int>::iterator it = adyacentesAi.begin();
     bool res = false;
 
-    //  Si no llegue al final de los vecinos de i    y      (si esta en la queue           o         ya fue recorrido)
-    while ((adyacente != adyacentesAi.end()) && (res != true))
-    {
-        if ((elementosEnQueue.find(*adyacente) == elementosEnQueue.end()) && (visitado[*adyacente] == -1))
+    //Recorro adyacentesAi mientras que res sea false 
+    while ((it != adyacentesAi.end()) && (res == false))
+    {   //  Si no esta en la lista        y     no fue visitado
+        if ((elementosEnQueue[*it] == -1) && (visitado[*it] == -1))
         {
-            j = *adyacente;
+            j = *it;
             res = true;
         }
-
-        adyacente++;
+        it++;
     }
+
+    if (res == true)
+    {
+        it--;
+        adyacentesAi.erase(it);
+    }
+    
 
     return res;
 }
 
-void bfs(unordered_map<long long int, set<long long int>> &lAdya, vector<long long int> &visitado, vector<long long int> &dist){
-        
-    long long int r = 1;                // Elegir un vertice 1 como raiz
+void bfs(vector<set<long long int>> &lAdyacencias, vector<long long int> &visitados, vector<long long int> &dist){
     
-    visitado[r] = 1;                    // Seteo a la raiz como visitada
+    visitados[0] = 1;                    // Seteo a la raiz como visitada
+
+    queue<long long int> lista;         // Defino la lista para hacer BFS
+
+    vector<long long int> copiaLista(dist.size());
     
-    queue<long long int> queue;         // Defino la queue para hacer BFS
-    unordered_set<long long int> copia;
-
-    queue.push(r);           // Meto la raiz dentro de la queue O(1)
-    copia.insert(r);
-
-    while (!queue.empty())   // Mientras que la queue no este vacia
+    for (int i = 0; i < copiaLista.size(); i++)
     {
-        long long int i = queue.front(); // Elijo un nodo i de la queue   O(1)
+        copiaLista[i] = -1;
+    }
+    
+    lista.push(0);           // Meto la raiz dentro de la lista O(1)
+    copiaLista[0] = 1;
 
-        //Si existe un arco (i, j) tal que j /∈ queue entonces
-        if (existeJQueNoEstaEnqueueYNoFueVisitado(copia, lAdya.at(i), visitado))   //Si un j adyacente a i no esta en la queue
+    while (!lista.empty())   // Mientras que la lista no este vacia
+    {
+        long long int i = lista.front(); // Elijo un nodo i de la lista   O(1)
+
+        //Si existe un arco (i, j) tal que j /∈ lista entonces
+        if (existeJ(lAdyacencias[i], copiaLista, visitados))   //Si un j adyacente a i no esta en la lista
         {
             dist[j] = dist[i] + 1;
-            visitado[j] = 1;        //visitado[j] = 1
-            queue.push(j);          //LISTA ← LISTA ∪ {j}
-            copia.insert(j);
+            visitados[j] = 1;        //visitado[j] = 1
+            lista.push(j);          //LISTA ← LISTA ∪ {j}
+            copiaLista[j] = 1;
         }else{
-            queue.pop();
-            copia.erase(i);
+            lista.pop();
+            copiaLista[i] = -1;
         }
     }
 }
 
 
 int main() {
-    
+
     //Codigo para hacer la entrada y salida de datos mas eficiente
     ios::sync_with_stdio(0);
     cin.tie(0);
 
     long long int n, u, v;
 
-    cin >> n;    
+    cin >> n;
 
-    vector<long long int> visitados (n + 1);
-    for (long long int i = 0; i < (n + 1); i++)
-    {
-        visitados[i] = -1;
-    }
-
-    vector<long long int> dist (n + 1);
-    for (long long int i = 0; i < (n + 1); i++)
-    {
-        if (i != 1)
-        {
-            dist[i] = -1;
-        }else{
-            dist[i] = 0;
-        }
-    }
-
-    //Solo funciona para arboles, por lo tanto solo recibe (n - 1) aristas.
-    unordered_map<long long int, set<long long int>> listaAdyacencias;
-
-    for (long long int i = 0; i < n; i++)
-    {
-        listaAdyacencias.insert( make_pair<long long int, set<long long int>>((i + 1), {}));
-    }
-    
 
     if (n <= 1)
     {
         cout << 0 << "\n";
     }else{
+        
+        vector<set<long long int>> lAdyacencias(n);
+
         for (long long int i = 0; i < (n - 1); i++)       //O(n.log(n))
         {
             cin >> u >> v;
-            listaAdyacencias.at(u).insert(v);
-            listaAdyacencias.at(v).insert(u);
+            lAdyacencias[u - 1].insert(v - 1);
+            lAdyacencias[v - 1].insert(u - 1);
+        }
+
+
+        vector<long long int> visitados (n);
+
+        for (long long int i = 0; i < n; i++)
+        {
+            visitados[i] = -1;
+        }
+
+
+        vector<long long int> dist (n);
+
+        for (long long int i = 1; i < n; i++)   //dist[0] = 0
+        {
+            dist[i] = -1;
         }
 
         //Solo reciben arboles
-        bfs(listaAdyacencias, visitados, dist);
+        bfs(lAdyacencias, visitados, dist);
 
         long long int count = 0;
 
-        for (long long int i = 1; i < (n + 1); i++)
+        for (long long int i = 0; i < n; i++)
         {
             if((dist[i] % 2) == 0){
                 count++;
