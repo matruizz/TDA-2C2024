f888ffc0ffa8be3320272957cd7a057e11bfe039
matruizz <mateoruizz1299@gmail.com>
2025-06-05 22:49:09 -0300

    Versiones finales de todos los ejercicios.

--- tp1/Aggressive cows/aggressive.cpp
+++ tp1/Aggressive cows/aggressive.cpp
@@ -1,19 +1,19 @@
-
- //Incluye la libreria standar completa... iostream, algorithm, vector
+//Incluye la libreria standar completa... iostream, algorithm, vector
 #include <bits/stdc++.h>
-
+ 
 using namespace std;
-
+ 
 int t = 0, n = 0, c = 0, minDistance = 1000000000, maxDistance = 0;
-
+ 
 bool cow(vector<int> &stalls, int &cows, int &minMaxDistance){
-
+    
+ 
     int vacas = cows;
-
+ 
     vacas--;
-
+    
     int ult = 0, act = 0;
-
+    
     while ((act < stalls.size()) && (vacas > 0))
     {
         if ((stalls[act] - stalls[ult]) >= minMaxDistance)
@@ -28,41 +28,55 @@ bool cow(vector<int> &stalls, int &cows, int &minMaxDistance){
         return true;
     }
     
+ 
     return false;
 }
-
+ 
 int aggresive(vector<int> &stalls, int &cows){
-
-    int aux = 1;
-
-    while(cow(stalls, cows, aux)) {
-        aux++;
+ 
+    int minMaxDistance = (int) std::round(((minDistance + maxDistance) * 1.0) / 2);
+ 
+    while((minDistance + 1 ) < maxDistance) {
+ 
+        if (cow(stalls, cows, minMaxDistance))
+        {
+            minDistance = minMaxDistance;
+        }else{
+            maxDistance = minMaxDistance;
+        }
+ 
+        minMaxDistance = (int) std::round(((minDistance + maxDistance) * 1.0) / 2);
     }
-    aux--;
-    return aux;
+    
+    if (cow(stalls, cows, minDistance))
+    {
+        return minDistance;
+    }else{
+        return minDistance - 1;
+    }
 }
-
-
+ 
+ 
 int main() {
-
+ 
     //Codigo para hacer la entrada y salida de datos mas eficiente
     ios::sync_with_stdio(0);
     cin.tie(0);
-
+ 
     cin >> t;
-    
+ 
     vector<int> res (t);
-
+ 
     for (int i = 0; i < t; i++)
     {
         cin >> n >> c;
-
+ 
         vector<int> stalls(n);
-
+ 
         for (int j = 0; j < n; j++)
         {
             cin >> stalls[j];
-
+ 
             if (stalls[j] > maxDistance)
             {
                 maxDistance = stalls[j];
@@ -74,15 +88,15 @@ int main() {
         vector<int>::iterator start = stalls.begin();
         vector<int>::iterator finish = stalls.end();
         sort(start, finish);
-
+ 
         res[i] = aggresive(stalls, c);
-
+ 
         minDistance = 1000000000;
         maxDistance = 0;
     }
     for (int i = 0; i < t; i++)
     {
-        cout << res[i] << endl;
+        cout << res[i] << "\n";
     }
     
-}
+} 
--- tp1/I am very busy/busy.cpp
+++ tp1/I am very busy/busy.cpp
@@ -1,20 +1,19 @@
-
  //jncluye la libreria standar completa... iostream, algorithm, vector
 #include <bits/stdc++.h>
-
+ 
 using namespace std;
-
-
+ 
+ 
 bool comp(pair<int, int> x, pair<int, int> y){
     return (x.second < y.second);
 }
-
-
+ 
+ 
 int busy(vector<pair<int, int> > v){
-
+ 
     int res = 1;
     int ult = 0;    //Indice del ultimo intervalo que se conto
-
+ 
     for (int i = 1; i < v.size(); i++)
     {
         if (v[ult].second <= v[i].first)
@@ -23,58 +22,47 @@ int busy(vector<pair<int, int> > v){
             res++;
         }
     }
-
+ 
     return res;
 }
-
-
+ 
+ 
 int main() {
-
+ 
     //Codigo para hacer la entrada y salida de datos mas eficiente
     ios::sync_with_stdio(0);
     cin.tie(0);
-
+ 
     int t = 0, n = 0;
     
     cin >> t;   //# Tests cases 1 <= t <= 10
-
+ 
     vector<vector<pair<int, int> > > testCase(t);
-
+ 
     for (int i = 0; i < t; i++)
     {
-
+ 
         cin >> n;   //# Actividades del test case 1 <= N <= 100000
-
+ 
         for (int j = 0; j < n; j++)
         {
             //0 <= start < end <= 1000000
             int s, f;
             cin >> s >> f;
-
+ 
             testCase[i].push_back(make_pair(s,f));
         }
-
+ 
         vector<pair<int, int> >::iterator inicio = testCase[i].begin();
         vector<pair<int, int> >::iterator fin = testCase[i].end();
-
+ 
         sort(inicio, fin, comp);
     }
-
+ 
     cout << endl;
-
+ 
     for (int i = 0; i < testCase.size(); i++)
     {
         cout << busy(testCase[i]) << endl;
     }
-
-
-
-    /*for (int i = 0; i < testCase.size(); i++)
-    {
-        for (int j = 0; j < testCase[i].size(); j++)
-        {
-            cout << testCase[i][j].first << ", " << testCase[i][j].second << endl;
-        }
-        cout << endl;
-    }*/
-}
+}  
